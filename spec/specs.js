044349d71f244ff37ef22a99691c455aaf84b0e5
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-09-18 10:58:52 -0700

    Add pricing and topping functions to pass second test.

--- spec/specs.js
+++ spec/specs.js
@@ -2,10 +2,19 @@ describe('Pizza', function() {
   it("creates a pizza with the given selections of size and toppings", function() {
     var quantity = 1;
     var pizzaSize = ["Medium", 12];
-    var pizzaToppings = ["cheese", "pineapple"];
-    var testPizza = new Pizza(quantity, pizzaSize, pizzaToppings);
+    var pizzaTopping = ["cheese", "pineapple"];
+    var testPizza = new Pizza(quantity, pizzaSize, pizzaTopping);
     expect(testPizza.quantity).to.equal(1);
     expect(testPizza.pizzaSize).to.eql(["Medium", 12]);
-    expect(testPizza.pizzaToppings).to.eql(["cheese", "pineapple"]);
+    expect(testPizza.pizzaTopping).to.eql(["cheese", "pineapple"]);
   });
+
+  it("calculates the cost of a pizza using the given selections", function() {
+    var testQuantity = 2;
+    var testPizzaSize = new pizzaSize("Medium", 12);
+    var testPizzaTopping1 = new Topping("cheese", 1);
+    var testPizzaTopping2 = new Topping("pineapple", 3);
+    var testPizza = new Pizza(testQuantity, testPizzaSize, [testPizzaTopping1, testPizzaTopping2]);
+    expect(testPizza.price()).to.equal(32);
+    });
 });
